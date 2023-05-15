f60942b9f21c095986a31c1c7fd11b62a87f91fe
Reee22 <93215371+Ree6e@users.noreply.github.com>
2023-05-16 00:44:56 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,10 +6,10 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="TECNO-KE5k" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_tecno_TECNO-KE5k/tree/full_ke5k_h6122-user-10-QP1A.190711.020-117739-release-keys" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export VENDOR="TECNO" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
