89bcf04b0d94deaa0512bf8aabb3d38981856013
Reee22 <93215371+Ree6e@users.noreply.github.com>
2023-09-04 20:44:45 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,15 +3,15 @@
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-10" # the branch of manifest
+export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="TECNO-KE5k" # codename used in device tree
-export DT_LINK="https://github.com/twrpdtgen/android_device_tecno_TECNO-KE5k/tree/full_ke5k_h6122-user-10-QP1A.190711.020-117739-release-keys" # device tree link
-export DT_BRANCH="twrp" # device tree branch
+export DEVICE="KE5k" # codename used in device tree
+export DT_LINK="https://github.com/Ree6e/Twrp_device_KE5k/" # device tree link
+export DT_BRANCH="11" # device tree branch
 export VENDOR="TECNO" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="twrp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
